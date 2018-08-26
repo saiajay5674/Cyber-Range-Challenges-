1c4c0f3add55b145c7a8fce13efe808da82a46cb
Sai Ajayakumar <saiajay5674@gmail.com>
2018-08-26 13:23:18 -0400

    Bash Script with User Prompts

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 #
 #
-# Login ID
-LOGIN=(replace with login id)
-# IP address
-IP=(replace with server ip address)
+echo -e "Login ID\n"
+read LOGIN
+
+echo -e "IP Address\n"
+read IP
 
 LOGA=a.txt
 LOGB=b.txt
@@ -15,6 +16,7 @@ LOGB=b.txt
 ssh $LOGIN@$IP '(echo "Disk";df -h;\
  echo "DNS";grep nameserver /etc/resolv.conf;\
  echo "Memory";free|grep -B1 Mem;\
- echo "Kernel";uname -r)' >> a.txt
+ echo "Kernel";uname -r)' >> $LOGA
 
 tr ' ' ',' <$LOGA >$LOGB
+
